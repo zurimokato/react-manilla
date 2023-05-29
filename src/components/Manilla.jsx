c422fbef2eac6aa4337fb116fb1fdb972c997287
zurimokato <noj2304@gmail.com>
2023-05-28 20:07:20 -0500

    eliminar del carrito conversion de moneda

--- src/components/Manilla.jsx
+++ src/components/Manilla.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { db } from '../firebase'
-import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc, query, orderBy, where, getDoc, getDocs } from 'firebase/firestore'
+import { collection, query, where, getDocs } from 'firebase/firestore'
 
 function Manilla() {
     const [material, setMaterial] = useState("");
     const [tipo, setTipo] = useState("");
     const [dije, setDije] = useState("");
     const [manillas, setManillas] = useState([]);
-    const [totalCarrito, setTotalCarrito] = useState(0);
+    const [conversion, setConversion] = useState(1);
+    const [moneda, setMoneda] = useState(1);
 
     const addCarrito = async e => {
         e.preventDefault();
@@ -31,6 +32,9 @@ function Manilla() {
         const querySnapshot = await getDocs(q);
         const manis = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id, cantidad: 1 }));
         agregarManilla(manis[0]);
+        setMaterial("");
+        setDije("");
+        setTipo("");
 
 
     }
@@ -46,11 +50,10 @@ function Manilla() {
     }
 
     const calcularTotal = () => {
-        return manillas.reduce((total, producto) => total + producto.valor * producto.cantidad, 0);
+        return manillas.reduce((total, producto) => total + producto.valor * producto.cantidad*conversion, 0);
     }
 
     const cambiarCantidad = (e, item) => {
-        console.log(e)
         item.cantidad = e.target.value;
         setManillas([...manillas]);
     }
@@ -59,13 +62,24 @@ function Manilla() {
     const eliminarDelCarrito = (productoId) => {
         const carritoActualizado = manillas.filter((p) => p.id !== productoId);
         setManillas(carritoActualizado);
-      };
+    }
+    const cambioMoneda = (value) => {
+        console.log(value);
+        setMoneda(value);
+        if(value==1){
+            setConversion(1);
+
+        }else if(value ==2){
+            setConversion(5000);
+        }
+
+    }
 
 
 
     return (
         <div className='container mt-5'>
-            <h1 className='text-center'>Manillas</h1>
+            <h1 className='text-center'>Manillas S.A.</h1>
             <hr />
             <div className="row">
                 <div className="col-4">
@@ -73,15 +87,15 @@ function Manilla() {
                     <form onSubmit={addCarrito}>
                         <div className="row">
                             <div className="col-3">
-                                <p className="small text-muted mb-4 pb-2">Material</p>
+                                <p className="small text-muted mb-1 pb-1">Material</p>
                                 <select name="select-material" id="material" className="form-select" value={material} onChange={(e) => setMaterial(e.target.value)}>
                                     <option value="">Seleccione un material</option>
-                                    <option value="cuero" >Cuero</option>
+                                    <option value="cuero">Cuero</option>
                                     <option value="cuerda">Cuerda</option>
                                 </select>
                             </div>
                             <div className="col-3">
-                                <p className="small text-muted mb-4 pb-2">Dije</p>
+                                <p className="small text-muted mb-1 pb-1">Dije</p>
                                 <select name="select-dije" id="dije" className="form-select" value={dije} onChange={(e) => setDije(e.target.value)}>
                                     <option value="">Seleccione un dije</option>
                                     <option value="martillo">Martillo</option>
@@ -89,7 +103,7 @@ function Manilla() {
                                 </select>
                             </div>
                             <div className="col-6">
-                                <p className="small text-muted mb-4 pb-2">Tipo</p>
+                                <p className="small text-muted mb-1 pb-1">Tipo</p>
                                 <select name="select-tipo" id="tipo" className="form-select" value={tipo} onChange={(e) => setTipo(e.target.value)}>
                                     <option value="">Seleccione un tipo</option>
                                     <option value="oro, oro rosado" >Oro, Oro rosado</option>
@@ -100,7 +114,7 @@ function Manilla() {
 
                         </div>
                         <div className="row mt-2">
-                            <div class="d-grid gap-2 col-6 mx-auto">
+                            <div className="d-grid gap-2 col-6 mx-auto">
                                 <button className='btn btn-primary btn-block'>
                                     Agregar al carrito</button>
                             </div>
@@ -113,14 +127,13 @@ function Manilla() {
                 </div>
                 <div className="col-7">
                     <h4 className="text-center">Carrito de Manillas</h4>
-
                     {
                         manillas.map(item => (
 
                             <div className="card mb-2" key={item.id}>
-                                <div className="card-body p-3">
-                                    <div className="row align-items-center">
-                                        <div className="col-md-1">
+                                <div className="card-body">
+                                    <div className="row">
+                                        <div className="col">
                                             {item.material == 'cuero' ? (
                                                 <>
                                                     <img src="https://http2.mlstatic.com/D_NQ_NP_626492-MCO51160975398_082022-O.jpg"
@@ -139,25 +152,25 @@ function Manilla() {
                                         </div>
                                         <div className="col-md-1 d-flex justify-content-center">
                                             <div>
-                                                <p className="small text-muted mb-4 pb-2">Material</p>
+                                                <p className="small text-muted mb-1 pb-1">Material</p>
                                                 <p className="lead fw-normal mb-0">{item.material}</p>
                                             </div>
                                         </div>
                                         <div className="col-md-2 d-flex justify-content-center">
                                             <div>
-                                                <p className="small text-muted mb-4 pb-2">Dije</p>
+                                                <p className="small text-muted mb-1 pb-1">Dije</p>
                                                 <p className="lead fw-normal mb-0"><i className="fas fa-circle me-2" style={{ color: '#fdd8d2' }}></i>{item.dije}</p>
                                             </div>
                                         </div>
                                         <div className="col-md-2 d-flex justify-content-center">
                                             <div>
-                                                <p className="small text-muted mb-4 pb-2">Tipo</p>
+                                                <p className="small text-muted mb-1 pb-1">Tipo</p>
                                                 <p className="lead fw-normal mb-0">{item.tipo}</p>
                                             </div>
                                         </div>
-                                        <div className="col-md-3 d-flex justify-content-center">
+                                        <div className="col-md-1 d-flex justify-content-center">
                                             <div>
-                                                <p className="small text-muted mb-4 pb-2">Cantidad</p>
+                                                <p className="small text-muted mb-1 pb-1">Cantidad</p>
                                                 <div className='input-group'>
                                                     <input className="form-control text-center" type="number" min={1} max={100} value={item.cantidad} onChange={e => { cambiarCantidad(e, item) }} />
                                                 </div>
@@ -166,20 +179,20 @@ function Manilla() {
                                         </div>
                                         <div className="col-md-1 d-flex justify-content-center">
                                             <div>
-                                                <p className="small text-muted mb-4 pb-2">Precio</p>
-                                                <p className="lead fw-normal mb-0">{item.valor}</p>
+                                                <p className="small text-muted mb-1 pb-1">Precio</p>
+                                                <p className="lead fw-normal mb-0">{item.valor*conversion}</p>
                                             </div>
                                         </div>
                                         <div className="col-md-1 d-flex justify-content-center">
                                             <div>
-                                                <p className="small text-muted mb-4 pb-2">Total</p>
-                                                <p className="lead fw-normal mb-0">{item.valor * item.cantidad}</p>
+                                                <p className="small text-muted mb-1 pb-1">Total</p>
+                                                <p className="lead fw-normal mb-0">{item.valor * item.cantidad*conversion}</p>
                                             </div>
                                         </div>
                                         <div className="col-md-1 d-flex justify-content-center">
-                                            <div>
-                                                <button type="button" class="btn btn-outline-danger" onClick={() => eliminarDelCarrito(item.id)}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                            <div className=' justify-content-center'>
+                                                <button type="button" className="btn btn-outline-danger" onClick={() => eliminarDelCarrito(item.id)}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                                                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
                                                     </svg>
                                                 </button>
@@ -193,6 +206,13 @@ function Manilla() {
 
                     <div className="card mb-5">
                         <div className="card-body p-4">
+                            <div className="float-start">
+                                <span className='small text-muted'>Moneda </span>
+                                <select className="form-select" name="select moneda" value={moneda} onChange={(e) => cambioMoneda(e.target.value)}>
+                                    <option value="1">USD</option>
+                                    <option value="2">COP</option>
+                                </select>
+                            </div>
                             <div className="float-end">
                                 <p className="mb-0 me-5 d-flex align-items-center">
                                     <span className="small text-muted me-2">Order total:</span> <span
